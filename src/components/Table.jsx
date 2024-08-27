124edf94f05682bd57e4ed980ad6cc50f4838c4b
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-08-27 14:45:04 -0400

    Montado el proyecto

---
+++ src/components/Table.jsx
@@ -0,0 +1,20 @@
+const Table = () => {
+    return (
+        <>
+            <table className="project-table">
+                <thead>
+                    <tr>
+                        <th>Nombre de Proyecto</th>
+                        <th>Fecha Inicial</th>
+                        <th>Fecha Final</th>
+                        <th>Descripcion del Proyecto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Table;
