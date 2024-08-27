c02a794197b5b0bd7fe2f5d4aa61756a2d15c366
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-08-27 14:40:08 -0400

    Montado el Proyecto y Hecho los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Form from './components/Form'
+import Table from './components/Table'
 
 function App() {
 
@@ -9,18 +10,7 @@ function App() {
         <Form/>
       </div>
       <div className='right-container'>
-      <table className="project-table">
-        <thead>
-            <tr>
-                <th>Nombre de Proyecto</th>
-                <th>Fecha Inicial</th>
-                <th>Fecha Final</th>
-                <th>Descripcion del Proyecto</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
+        <Table/>
       </div>
     </div>
   )
